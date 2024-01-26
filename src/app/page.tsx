7df22a7b2417fbbf60a5501340b8b228afa1f48d
JamesDrysdale <jamesdrysdale84@gmail.com>
2024-01-26 12:48:05 +0000

    Brand name is using brand font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import styles from '@/app/ui/home.module.css';
 
 export default function Home() {
   return (
     <main className="">
-      
+      <h1 className={styles.brandname}>Odyssey Fleet</h1>
+      {/* <div className={styles.shape} /> */}
     </main>
   );
 }
---
+++ src/app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Oswald, Roboto } from 'next/font/google';
+
+export const oswald = Oswald({ subsets: ['latin'] });
+export const roboto = Roboto({ weight: ["400", "700"], subsets: ['latin'] });
